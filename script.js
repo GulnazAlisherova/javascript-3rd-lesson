790c3b93e69c651d08e7acf95022c2c5e9aa7d45
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-24 14:15:02 +0600

    added if

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var message = prompt("What's your messge?");
-if (message == "") {
-  message = "no message";   
-}
-alert 
+// var message = prompt("What's your messge?");
+// if (message == "") {
+//   message = "no message";   
+// }
+// alert (message);
+
+var year = prompt("When Kyrgyzstan gained independence?");
+if (year)
