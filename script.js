9512443a1d66d023160d1c9fe47505c1b57d5da9
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-24 15:03:12 +0600

    added currentnumber

--- script.js
+++ script.js
@@ -43,22 +43,30 @@
 // }
 
  
-var city = prompt("В каком городе ты живёшь");
+// var city = prompt("В каком городе ты живёшь");
 
-if (city === "Каракол") {
-  alert("Иссык-Куль");
-} else if (city === "Ош") {
-  alert("Ош");
-} else if (city === "Бишкек") {
-  alert("Чуй");
-} else if (city === "Ат-Башы") {
-  alert("Нарын");
-} else if (city === "Сузак") {
-  alert("Баткен");
-} else if (city === "Токтогул") {
-  alert("Жалал-Абад");
-} else if (city === "Ат-Башы") {
-  alert("Нарын");
-} else {
-  alert("Вы живете заграницей!");
+// if (city === "Каракол") {
+//   alert("Иссык-Куль");
+// } else if (city === "Ош") {
+//   alert("Ош");
+// } else if (city === "Бишкек") {
+//   alert("Чуй");
+// } else if (city === "Ат-Башы") {
+//   alert("Нарын");
+// } else if (city === "Сузак") {
+//   alert("Баткен");
+// } else if (city === "Токтогул") {
+//   alert("Жалал-Абад");
+// } else if (city === "Ат-Башы") {
+//   alert("Нарын");
+// } else {
+//   alert("Вы живете заграницей!");
+// }
+
+
+var currnetNumber = 0 ;
+var stopNumber = 10;
+
+if (stopNumber < stopNumber) {
+currnetNumber = currnetNumber + 1;
 }
