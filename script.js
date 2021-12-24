31ee6af86b1fadd70df10f07746bee2f7fddd998
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-24 14:38:15 +0600

    added else if

--- script.js
+++ script.js
@@ -32,9 +32,12 @@
 // confirm();
  
 var year = prompt("What year Kyrgyzstan gained independence?");
-if (year === "1991"){
-  alert("Correct");
+if (year > 1991){
+  alert("This year is after independence day ");
+}
+else if (year === "1991"){
+  alert("You got it!");
 }
 else{
-  alert("Incorrect!");
+  alert("This year is before independence day!");
 }
